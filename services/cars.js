93bc360b5be402c3405d8c59c919848ce5e5cb2e
Michael Douglas <Michael Douglas>
2017-04-03 17:14:47 -0300

    commit inicial

---
+++ services/cars.js
@@ -0,0 +1,43 @@
+var Car = require('./../models/car');
+
+exports.find = function (req, res) {
+   Car.find({}, function (err, cars) {
+     if(err)
+         return;
+     res.render('cars_list', {cars: cars});
+   });
+
+};
+
+exports.new = function (req,res) {
+    res.render('cars_new');
+};
+
+exports.create = function (req, res) {
+    Car.create(req.body, function (err,car) {
+        if(err)
+            return;
+
+        res.redirect('/cars');
+    })
+};
+
+exports.edit = function (req, res) {
+    Car.findById(req.params.id, function (err, car) {
+        if(err)
+            return;
+
+        res.render('cars_edit',{car:car});
+    });
+
+}
+
+
+exports.update = function (req, res) {
+
+};
+
+exports.remove = function (req, res) {
+
+};
+
